42f536a00ca699da021ffaf6bed62fd50d16e07b
Mohsen Khezri <mohsenkhezri92@gmail.com>
2024-12-30 09:26:23 +0330

    change the hero image , logo & favico

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,15 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div
+          {/* <div
                className={styles.shape}
-          />
+          /> */}
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal `}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to TarnamaGostar.</strong> This is the example for the{' '}
+            <a href="https://tarnamagostar.ir" className="text-blue-500">
+              TarnmaGostar dashboard
             </a>
-            , brought to you by Vercel.
+            , brought to you .
           </p>
           <Link
             href="/login"
@@ -33,14 +33,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/hero-desktop2.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/hero-desktop2.png"
             width={560}
             height={620}
             className="block md:hidden"
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,21 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <Image
+                  src="/logo/tarnama.png"
+                  width={200}
+                  height={170}
+                  className="hidden md:block"
+                  alt="Screenshots of the dashboard project showing desktop version"
+                />
+      {/* <p className="text-[44px] text-white "></p> */}
     </div>
   );
 }
